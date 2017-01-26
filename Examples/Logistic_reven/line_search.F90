28cbf99cc90b91abcc19271628091960f6200fb1
Your Name <you@example.com>
2017-01-26 13:22:39 -0500

    modified:   Logistic/Main1.F90 	modified:   Logistic/line_search.F90 	modified:   Logistic/log_eq.F90 	modified:   Logistic/norma.F90 	modified:   Logistic_reven/Main1.F90 	modified:   Logistic_reven/Start.F90 	modified:   Logistic_reven/line_search.F90 	modified:   Logistic_reven/log_eq.F90 	modified:   Logistic_reven/norma.F90 	modified:   Logistic_reven/output.F90 	modified:   SelfAdjoint/lu.f90

--- Examples/Logistic_reven/line_search.F90
+++ Examples/Logistic_reven/line_search.F90
@@ -1,12 +1,12 @@
 module line_searchs
-
-
+  
+  
   use norma0
   private compute1,compute2
   interface compute
      module procedure compute1,compute2
   end interface compute
-contains
+  contains
   recursive function h_mul(k,nn,nmax,yy,xx,x0) result(ret)
     use norma0
     !  use prog_bar
@@ -14,9 +14,9 @@ contains
     double precision,dimension(nmax,nn)::yy,xx
     doubleprecision::ret(nn),ret0(nn),ret1(nn),alpha
     doubleprecision,intent(in)::x0(nn)
-
+    
     integer m,n,p
-
+    
     if (k.eq.1) then
        ret=x0;
     else
@@ -40,7 +40,7 @@ contains
        ret=ret+ret0/alpha
     endif
   end function h_mul
-
+  
   subroutine compute1(x,f,grad)
     use OAD_active
     use OAD_tape
@@ -55,45 +55,31 @@ contains
     type(active) :: L
     type(active) ::ad_cost0
     integer N
-    N=2
-  revStatsRevolveCPcount=N
-  call oad_tape_init()
-!  call revStatsInit()
-    dt=0.01
-    x0%v=x(1)
-    x0%d=0
-    l%v=x(2)
-    l%d=0
-    k%v=x(3)
-    k%d=0
-    AD_COST0%d=1.0
-    AD_COST0%v=0.0
-       our_rev_mode%arg_store=.FALSE.
-     our_rev_mode%arg_restore=.FALSE.
-     our_rev_mode%res_store=.FALSE.
-     our_rev_mode%res_restore=.FALSE.
-     our_rev_mode%plain=.FALSE.
-     our_rev_mode%tape=.TRUE.
-     our_rev_mode%adjoint=.TRUE.
-     call AD_COST(X0, K, L, DT, N, AD_COST0)
-     
-    grad(1)=x0%d
-    grad(2)=l%d
-    grad(3)=k%d
+    N=10;dt=0.01
+    revStatsRevolveCPcount=N
+    call oad_tape_init()
+    call revStatsInit()
     
-    AD_COST0%d=1.0
-    AD_COST0%v=0.0
-       our_rev_mode%arg_store=.FALSE.
-     our_rev_mode%arg_restore=.FALSE.
-     our_rev_mode%res_store=.FALSE.
-     our_rev_mode%res_restore=.FALSE.
-     our_rev_mode%plain=.TRUE.
-     our_rev_mode%tape=.FALSE.
-     our_rev_mode%adjoint=.FALSE.
-     call AD_COST(X0, K, L, DT, N, AD_COST0)
-    f=AD_COST0%v
+    call OAD_revPlain()
+    call zero_deriv(x_curr)
+    call zero_deriv(k_act)
+    call zero_deriv(l_act)
+    call setup_par(x(1),x(2),x(3),dt,30)
+    
+    summ%d=1.0
+    summ%v=0.0
+    call OAD_revTape()
+    call AD_COST()
+    f=summ%v
+    
+    call OAD_revAdjoint()
+    call AD_COST()
+    grad(1)=xcurr%d
+    grad(2)=l_act%d
+    grad(3)=k_act%d
+    
   end subroutine compute1
-
+  
   subroutine compute2(nn,x0,f,grad,ctrlname,costname,direction)
     !USE GRID
     implicit none
@@ -111,7 +97,7 @@ contains
           return
        endif
     enddo
-
+    
     !#ifdef DEBUG
     !call system("del d:/intel/inp.txt  ")
     !call system("del d:/intel/out.txt  ")
@@ -120,30 +106,30 @@ contains
     else
        stop
     endif
-
-
+    
+    
     if (my_is_nan(f)) then
        f=1e+38
        grad=1e+38*direction
        return
     endif
     do k=1,nn
-
+       
        if (my_is_nan(grad(k))) then
-
+          
           grad(1:nn)=1e+38*direction
           f=1e+38
           return
        endif
     enddo
-
-
-
+    
+    
+    
   end subroutine compute2
-
-
+  
+  
 #undef VALUE_BASED
-
+  
   subroutine LINE_SEARCH(NN,X0,GRAD0,F0,DIRECTION,EPS0,NITER,ctrlname,costname,if_restart,if_exceed,ncomp)
     integer ,intent(in)::nn
     INTEGER,INTENT(IN) ::NITER
@@ -158,12 +144,12 @@ contains
 #else
     call LINE_SEARCH1(NN,X0,GRAD0,F0,DIRECTION,EPS0,NITER,ctrlname,costname,if_restart,if_exceed,ncomp)
 #endif    
-
-
+    
+    
   end subroutine LINE_SEARCH
-
+  
   subroutine LINE_SEARCH2(NN,X0,GRAD0,F0,DIRECTION,EPS0,NITER,ctrlname,costname,if_restart,if_exceed,ncomp)
-
+    
     use norma0
     implicit none
     integer ,intent(in)::nn
@@ -189,7 +175,7 @@ contains
     enddo
     ffa=grada.x.direction;g0=ffa
     write(0,114) "initial a " ,a,fa,ffa,norma(direction)
-
+    
 777 call compute(nn,x0+eps0*direction,fb,gradb,ctrlname,costname,direction)
     ffb=gradb.x.direction;
     write(0,114) "initial b " ,b,fb,ffb,norma(gradb)
@@ -259,14 +245,14 @@ contains
        gamma=-1.0;
        goto 10
     endif
-
+    
     write(0,111) "initial a " ,a,fa,ffa
-
+    
     !    call compute(nn,x0,fa,grada,ctrlname,costname,direction)
     call compute(nn,x0+eps0*gamma*direction,fb,gradb,ctrlname,costname,direction)
     ffb=gradb.x.direction;
     write(0,111) "initial b " ,b,fb,ffb
-
+    
 30  do while(((ffb*ffa.ge.0)))
        if (ffa.eq.ffb) then
           b0=b*10
@@ -283,18 +269,18 @@ contains
        endif
        write(0,'(a,E16.8E2)')"Приближение изоляции минимума",b0
        b=b0
-
+       
        call compute(nn,x0+b*gamma*direction,fb,gradb,ctrlname,costname,direction)
-
+       
        ffb=gamma*(gradb.x.direction);
        write(0,111) "initial b " ,b,fb,ffb
        counter=counter+1
     enddo
-
+    
 40  if_suff=(((fa<0.9*f0).and.(wolfe_condition0(grada,direction))).or.((wolfe_condition0(gradb,direction)).and.(fb<0.9*f0)))
     do  while((.not. if_suff))
        c=davidon(a,b,fa,fb,ffa,ffb)
-
+       
        if ((ffa.le.0).and.((c-a)*(c-b)<0)) then
           if (min(abs(a-c),abs(b-c))<1e-4*abs(a-b)) then
              if (abs(a-c)<abs(b-c)) then
@@ -311,10 +297,10 @@ contains
           if ((my_is_nan(c)).or.(min(abs(a-c),abs(b-c))<1e-12*abs(a-b)).or.((c-a)*(c-b)>0)) then
              c=(a+b)/2
           endif
-
+          
        endif
        write(0,'(a,E16.8E2)')"Интерполяция давидона дала",c
-
+       
        call compute(nn,x0+c*gamma*direction,fc,gradc,ctrlname,costname,direction) 
        ffc=gradc.x.direction;
        counter=counter+1
@@ -323,7 +309,7 @@ contains
        write(0,111) "a " ,a,fa,ffa
        write(0,111) "b " ,b,fb,ffb
        write(0,112) counter,"c" ,c,fc,ffc
-
+       
        if (ffa*ffb<0) then
           if (ffa*ffc<0) then
              b=c;gradb=gradc;ffb=ffc;fb=fc
@@ -339,13 +325,13 @@ contains
                    A=C0;GRADA=GRADC0;FFA=FFC0;FA=FC0
                 ELSEIF (FFB*FFC0>0) THEN
                    B=C0;GRADB=GRADC0;FFB=FFC0;FB=FC0
-
+                   
                 ENDIF
              else
                 a=c;grada=gradc;ffa=ffc;fa=fc
              endif
           endif
-
+          
        else
           if (ffa*ffc<0) then
              b=c;gradb=gradc;ffb=ffc;fb=fc
@@ -358,24 +344,24 @@ contains
              else
                 b=c;gradb=gradc;ffb=ffc;fb=fc
              endif
-
+             
           endif
-
+          
        endif
     enddo
 300 if (fa<fb) then
        f0=fa;grad0=grada;x0=x0+a*gamma*direction
-
+       
     else
        f0=fb;grad0=gradb;x0=x0+b*gamma*direction
     endif
-
-
-
-
+    
+    
+    
+    
     ncomp=ncomp+counter
   END SUBROUTINE LINE_SEARCH0
-
+  
   subroutine LINE_SEARCH1(NN,X0,GRAD0,F0,DIRECTION,EPS0,NITER,ctrlname,costname,if_restart,if_exceed,ncomp)
     use norma0
     implicit none
@@ -404,20 +390,20 @@ contains
        gamma=-1.0;
        goto 10
     endif
-
+    
     write(0,111) "initial a " ,a,fa,ffa
     if (if_restart) then
        INQUIRE (FILE='linesearch.sav', EXIST=I_EXIST) 
        if (I_EXIST) then 
           if_restart=.false.
           open(FILE="linesearch.sav",unit=171,action='read',ERR=10)
-
+          
           read(171,*)i
           close(171)
           if_restart=.true.
           write(0,'(a)')"Штатный Перезапуск"
           if (i.eq.1) then
-
+             
              call restore_res(nn,a,grada,fa,'a',if_restart)
              if (.not.if_restart) then
                 write(0,'(a)')"Точка восстановления не найдена. Перезапуск"
@@ -455,12 +441,12 @@ contains
           if_restart=.false.
           goto 10
        endif
-
+       
     else
        !       call  dump_res(nn,0.0,grad0,f0,'a')
        call system("sync")
     endif
-
+    
     !    call compute(nn,x0,fa,grada,ctrlname,costname,direction)
     call compute(nn,x0+eps0*gamma*direction,fb,gradb,ctrlname,costname,direction)
     ffb=gradb.x.direction;
@@ -483,7 +469,7 @@ contains
        endif
        write(0,'(a,E16.8E2)')"Приближение изоляции минимума",b0
        b=b0
-
+       
        call compute(nn,x0+b*gamma*direction,fb,gradb,ctrlname,costname,direction)
        call  dump_res(nn,b,gradb,fb,'b')
        ffb=gamma*(gradb.x.direction);
@@ -496,11 +482,11 @@ contains
        call  dump_res(nn,b,gradb,fb,'b')
        call system("sync")
     enddo
-
+    
 40  if_suff=(((fa<0.9*f0).and.(wolfe_condition(f0,g0,fa,ffa,a))).or.((wolfe_condition(f0,g0,fb,ffb,b)).and.(fb<0.9*f0)))
     do  while((.not. if_suff))
        c=davidon(a,b,fa,fb,ffa,ffb)
-
+       
        if ((ffa.le.0).and.((c-a)*(c-b)<0)) then
           if (min(abs(a-c),abs(b-c))<1e-12*abs(a-b)) then
              if (abs(a-c)<abs(b-c)) then
@@ -517,10 +503,10 @@ contains
           if ((my_is_nan(c)).or.(min(abs(a-c),abs(b-c))<1e-12*abs(a-b)).or.((c-a)*(c-b)>0)) then
              c=(a+b)/2
           endif
-
+          
        endif
        write(0,'(a,E16.8E2)')"Интерполяция давидона дала",c
-
+       
        call compute(nn,x0+c*gamma*direction,fc,gradc,ctrlname,costname,direction) 
        ffc=gradc.x.direction;
        counter=counter+1
@@ -529,7 +515,7 @@ contains
        write(0,111) "a " ,a,fa,ffa
        write(0,111) "b " ,b,fb,ffb
        write(0,112) counter,"c" ,c,fc,ffc
-
+       
        if (ffa*ffb<0) then
           if (ffa*ffc<0) then
              b=c;gradb=gradc;ffb=ffc;fb=fc
@@ -545,13 +531,13 @@ contains
                    A=C0;GRADA=GRADC0;FFA=FFC0;FA=FC0
                 ELSEIF (FFB*FFC0>0) THEN
                    B=C0;GRADB=GRADC0;FFB=FFC0;FB=FC0
-
+                   
                 ENDIF
              else
                 a=c;grada=gradc;ffa=ffc;fa=fc
              endif
           endif
-
+          
        else
           if (ffa*ffc<0) then
              b=c;gradb=gradc;ffb=ffc;fb=fc
@@ -564,9 +550,9 @@ contains
              else
                 b=c;gradb=gradc;ffb=ffc;fb=fc
              endif
-
+             
           endif
-
+          
        endif
        open(FILE="linesearch.sav",unit=171,action='write',status='replace',ERR=10)
        write(171,*)2
@@ -577,14 +563,14 @@ contains
     enddo
 300 if (fa<fb) then
        f0=fa;grad0=grada;x0=x0+a*gamma*direction
-
+       
     else
        f0=fb;grad0=gradb;x0=x0+b*gamma*direction
     endif
-
-
-
-
+    
+    
+    
+    
     call system("rm *.txt")
     call system("rm linesearch.sav")
     call system("sync")
@@ -618,51 +604,51 @@ contains
        gamma=-1.0;
        goto 101
     endif
-
+    
     write(0,111) "initial a " ,a,fa,ffa
-
+    
     !    call compute(nn,x0,fa,grada,ctrlname,costname,direction)
     call compute(nn,x0+eps0*gamma*direction,fb,gradb,ctrlname,costname,direction)
     ffb=gamma*(gradb.x.direction);
     write(0,111) "initial b " ,b,fb,ffb
     call system("sync")
     if ((fb.le.f0)) then
-
+       
        if (fb.eq.f0) then
           return
        endif
-
+       
        f0=fb;grad0=gradb;x0=x0+b*gamma*direction;eps0=b*gamma
        if_exceed=.true.
        return
     endif
-
+    
 401 if (ffb>0) then
        c=davidon(a,b,fa,fb,ffa,ffb)
     else
        c=a+0.2*(b-a)
     endif
-
+    
     call compute(nn,x0+c*gamma*direction,fc,gradc,ctrlname,costname,direction)
     ffc=gamma*(gradc.x.direction);
     write(0,111) "a " ,a,fa,ffa
     write(0,111) "b " ,b,fb,ffb
     write(0,111) "c " ,c,fc,ffc
     if ((fc<f0)) then
-
+       
        f0=fc;grad0=gradc;x0=x0+c*gamma*direction;eps0=c*gamma
        return
     endif
     b=c;gradb=gradc;ffb=ffc;fb=fc
-
-
-
-
+    
+    
+    
+    
     goto 401
-
-
+    
+    
   END SUBROUTINE LINE_SEARCH3
-
+  
   DOUBLEPRECISION function davidon(a,b,fa,fb,ffa,ffb)
     ! f1 - f(x1),f3 -f'(x3)
     ! f2 -f(x2),f4 -f'(x4)
@@ -671,16 +657,16 @@ contains
     DOUBLEPRECISION::a0,b0,c0,x1,x2
     a0=(b*ffb-a*ffb+b*ffa-a*ffa-2*fb+2*fa)/(b-a)**3
     b0=-(b**2*ffb+a*b*ffb-2*a**2*ffb+2*b**2*ffa-a*b*ffa-a**2*ffa-3*b*fb-&
-         &3*a*fb+3*b*fa+3*a*fa)/(b-a)**3
+    &3*a*fb+3*b*fa+3*a*fa)/(b-a)**3
     c0=(2*a*b**2*ffb-a**2*b*ffb-a**3*ffb+b**3*ffa+a*b**2*ffa-2*a**2*b*ff&
-         &a-6*a*b*fb+6*a*b*fa)/(b-a)**3
+    &a-6*a*b*fb+6*a*b*fa)/(b-a)**3
     if (a0.eq.0*a0) then
        davidon=a
     endif
     if (b0**2-a0*c0.lt.0) then
        davidon=a
     endif
-
+    
     x1=(-b0+sqrt(b0**2-a0*c0))/(a0)
     x2=(-b0+sqrt(b0**2-a0*c0))/(a0)
     if (6*a0*x1+2*b0>0) then
@@ -688,28 +674,28 @@ contains
     else
        davidon=x2
     endif
-
+    
   end function davidon
   logical function wolfe_condition0(grad1,dir)
     doubleprecision,intent(in)::grad1(:),dir(:)
     doubleprecision,parameter::alpha2=1e-3,alpha1=0.5e-4
-
+    
     wolfe_condition0=(-grad1.x.dir).ge.alpha2*norma(grad1)*norma(dir)
-
-
-
+    
+    
+    
   end function wolfe_condition0
-
+  
   logical function wolfe_condition(f0,g0,f1,g1,alpha)
     doubleprecision,intent(in)::f0,g0,f1,g1,alpha
     doubleprecision,parameter::alpha2=1e-3,alpha1=0.5e-4
-
+    
     wolfe_condition=(g1.ge.alpha2*g0).and.(f1 .le. f0 + alpha1*alpha*g0)
-
-
-
+    
+    
+    
   end function wolfe_condition
-
+  
   function Ak(m,nn,k,g,x,q,z)
     use norma0
     integer m,nn
@@ -723,7 +709,7 @@ contains
        summ=0
        do j=1,nn
           summ=summ+z(j)*(((x(k+1,i)-x(k,i)-q(k,i))*(x(k+1,j)-x(k,j))+(x(k+1,i)-x(k,i))*(x(k+1,j)-x(k,j)-q(k,j)))/alpha&
-               &+ beta*(x(k+1,i)-x(k,i))*(x(k+1,j)-x(k,j)))
+          &+ beta*(x(k+1,i)-x(k,i))*(x(k+1,j)-x(k,j)))
        enddo
        Ak(i)=summ
     enddo
@@ -732,27 +718,27 @@ contains
     integer,intent(in)::nn
     double precision,intent(in)::x,grad(nn),f
     character(len=*)::pref
-
+    
     open(unit=1943,file=pref//'_func_val.txt',action='write',status='replace')
     write(1943,*),f
     close(1943) 
-
-
-
+    
+    
+    
     open(unit=1943,file=pref//'_func_grad.txt',action='write',status='replace')
     write(1943,*),grad
     close(1943) 
-
-
+    
+    
     open(unit=1943,file=pref//'_func_x.txt',action='write',status='replace')
     write(1943,*),x
     close(1943) 
-
-
+    
+    
     open(unit=1943,file=pref//'_func_val.txt',action='write',status='replace')
     write(1943,*),f
     close(1943) 
-
+    
   end subroutine dump_res
   subroutine restore_res(nn,x,grad,f,pref,ifsucc)
     integer,intent(in)::nn
@@ -767,36 +753,36 @@ contains
     open(unit=1943,file=pref//'_func_val.txt',action='read',ERR=100)
     read(1943,*),f
     close(1943) 
-
+    
     inquire(file=pref//'_func_grad.txt',EXIST=ifsucc)
     if(.not.ifsucc) then
        return
     endif
-
+    
     open(unit=1943,file=pref//'_func_grad.txt',action='read',ERR=100)
     read(1943,*),grad
     close(1943) 
-
-
+    
+    
     inquire(file=pref//'_func_x.txt',EXIST=ifsucc)
     if(.not.ifsucc) then
        return
     endif
-
+    
     open(unit=1943,file=pref//'_func_x.txt',action='read',ERR=100)
     read(1943,*),x
     close(1943) 
-
-
+    
+    
     open(unit=1943,file=pref//'_func_val.txt',action='read')
     read(1943,*),f
     close(1943) 
-
+    
     return
 100 ifsucc=.false.
     return
   end subroutine restore_res
-
+  
 end module line_searchs
     module lb2
          
